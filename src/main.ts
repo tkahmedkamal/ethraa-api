1082c05cb742c7dd8efa924b40333e1d78d4796d
tkahmedkamal <tkahmedkamal@hotmail.com>
2024-09-13 19:10:09 +0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -31,17 +31,11 @@ async function bootstrap() {
 
   const PORT = app.get(ConfigService).get('PORT') || 4000;
   app.enableCors({
-    origin: frontendUrl,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    origin: [frontendUrl],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
     credentials: true,
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Authorization',
-    ],
-    preflightContinue: false,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
   });
   // app.enableCors();
   await app.listen(PORT);
