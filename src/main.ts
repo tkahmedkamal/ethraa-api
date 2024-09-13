45ebc8c88b109c029d0a7308acaad9f63ee72826
tkahmedkamal <tkahmedkamal@hotmail.com>
2024-09-13 19:24:08 +0300

    Update origin-cors

--- src/main.ts
+++ src/main.ts
@@ -34,8 +34,10 @@ async function bootstrap() {
     origin: ['*'],
     methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
     credentials: true,
+
+    preflightContinue: false,
     allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+      'Origin, X-Requested-With, Authorization, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
   });
   // app.enableCors();
   await app.listen(PORT);
